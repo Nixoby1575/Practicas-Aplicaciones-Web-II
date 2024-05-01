3ff3af6e28a31ab6378894f4b014dbbf27e1d632
Alexis Franco Llivipuma <105693703+Nixoby1575@users.noreply.github.com>
2024-04-30 22:23:18 -0500

    Add files via upload

---
+++ Practicas-P1/src/entities/TipoExamen.ts
@@ -0,0 +1,17 @@
+export const tipoExamen1 = {
+    id: 101,
+    descripcion: 'Examen de sangre',
+    indicaciones: 'Ayuno de 8 horas antes del examen'
+};
+
+export const tipoExamen2 = {
+    id: 102,
+    descripcion: 'Radiografía de tórax',
+    indicaciones: 'No es necesario ayunar'
+};
+
+export const tipoExamen3 = {
+    id: 103,
+    descripcion: 'Ecografía abdominal',
+    indicaciones: 'Tomar 1 litro de agua una hora antes del examen'
+};
